324383f9df20c013737f43e2b4464cd35f469594
myungsangBaek <bms6168@naver.com>
2022-06-30 21:57:57 +0900

    [FEATURE] Generate example2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useRef } from "react";
 
 function App() {
-  const canvasRef = useRef(null);
+  const canvas = useRef(null);
+
+  const draw = () => {
+    const ctx = canvas.current.getContext("2d");
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 50, 50);
+  };
 
   useEffect(() => {
-    const ctx = canvasRef.current.getContext("2d");
-    console.log(canvasRef.current.getContext("2d"));
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+    // const ctx = canvas.current.getContext("2d");
+    // ctx.fillRect(25, 25, 100, 100);
+    // ctx.clearRect(45, 45, 60, 60);
+    // ctx.strokeRect(50, 50, 50, 50);
+    draw();
   }, []);
 
   return (
     <div className="App">
-      <canvas ref={canvasRef} />
+      <canvas ref={canvas} />
     </div>
   );
 }
